1e23224eb2875f16719a64399c8bbd60a9e565b2
paekjongwoong <jeremy96kr@naver.com>
2025-10-20 16:27:50 +0900

    first commit

---
+++ src/main/java/org/example/project_board_pjw/ProjectBoardPjwApplication.java
@@ -0,0 +1,13 @@
+package org.example.project_board_pjw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBoardPjwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectBoardPjwApplication.class, args);
+    }
+
+}
